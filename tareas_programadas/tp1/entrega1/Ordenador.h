41fd7dd8abc1661f31abaea4482508fbb55dcfdd
eli <elizxbethhw@gmail.com>
2024-04-09 16:39:24 -0600

    Implementacion del selection y insertion sort

--- tareas_programadas/tp1/entrega1/Ordenador.h
+++ tareas_programadas/tp1/entrega1/Ordenador.h
@@ -13,8 +13,40 @@ class Ordenador{
 	// simplemente dejelos con el cuerpo vacio para evitar errores de compilacion, ya
 	// que se va a ejecutar el mismo main para todas las tareas.
     // Implemente los algoritmos en este archivo  (no en un CPP aparte).
-	void seleccion(int *A, int n);
-	void insercion(int *A, int n);
+	void seleccion(int *A, int n){
+		int menor;
+		int aux;
+		int i;
+		int j;
+
+		for (i = 1; i < n; i++){
+			menor = i;
+			for (int j = i+1; j < n; i++){
+				if (A[j] < A[i]){
+					menor = j;
+				}
+			}
+			aux = A[j];
+			A[j] = A[menor];
+			A[menor] = aux;
+		}
+	}
+
+	void insercion(int *A, int n){
+		int i, j, key;
+
+		for (i = 2; i <= n; i++){
+			key = A[i];
+			j = i - 1;
+			while (j > 0 && A[j] > key){
+				A[j+1] = A[j];
+				j = j - 1;
+			}
+			A[j + 1] = key;
+		}
+	}
+
+
 	void mergesort(int *A, int n);
 	void heapsort(int *A, int n);
 	void quicksort(int *A, int n);
