8fceeb032fa6ac4e542a362de37fc50986b96f97
Elizabeth Huang <cyhw.eli@Elizabeths-iMac.local>
2024-04-15 19:33:50 -0600

    Implementacion del Merge Sort y dos funciones adicionales para completarlo

--- tareas_programadas/tp1/entrega1/Ordenador.h
+++ tareas_programadas/tp1/entrega1/Ordenador.h
@@ -9,6 +9,43 @@ class Ordenador{
 	private:
     // Defina aqui los metodos auxiliares de los algoritmos de ordenamiento solamente.
     // Puede definir cuantos metodos quiera.
+
+	void ord_intercalacion(int * A, int * tmp, int izq, int der){
+		if (izq < der){
+      		int centro = (izq + der) / 2;
+      		ord_intercalacion (A, tmp, izq, centro);
+      		ord_intercalacion (A, tmp, centro + 1, der);
+			intercalar (A, tmp, izq, centro + 1, der);
+		}
+	}
+
+	void intercalar (int * A, int * tmp, int izq, int centro, int der){
+		int ap = izq, bp = centro, cp = izq;
+  		while ((ap < centro) && (bp <= der)){
+      		if (A[ap] <= A[bp]){
+   				tmp[cp] = A[ap];
+   				ap++;
+			} else {
+   				tmp[cp] = A[bp];
+   				bp++;
+			}
+			cp++;
+		}
+  		while (ap < centro){
+      		tmp[cp] = A[ap];
+      		cp++;
+      		ap++;
+		}
+  		while (bp <= der){
+      		tmp[cp] = A[bp];
+      		cp++;
+      		bp++;
+		}
+        for (ap = izq; ap <= der; ap++){
+           A[ap] = tmp[ap];
+		}
+}
+
 	
 	public:
 	Ordenador(){};
@@ -46,11 +83,15 @@ class Ordenador{
 		}
 	}
 
+	void mergesort(int *A, int n){
+		int *tmp = new int[n]; 
+  		ord_intercalacion (A, tmp, 0, n - 1);
+	}
+
 	std::string ImprimirDatosDeTarea(){
 		return "C23913 Tarea 1 Etapa 1";
 	}
 
-	void mergesort(int *A, int n);
 	void heapsort(int *A, int n);
 	void quicksort(int *A, int n);
 	void radixsort(int *A, int n);
