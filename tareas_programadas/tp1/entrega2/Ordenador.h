8a6b0cd6828c8da2c02607c8cf9519b1081a3f69
Elizabeth Huang <cyhw.eli@Elizabeths-iMac.local>
2024-04-28 17:47:03 -0600

    Implementacion del heapsort

--- tareas_programadas/tp1/entrega2/Ordenador.h
+++ tareas_programadas/tp1/entrega2/Ordenador.h
@@ -45,6 +45,29 @@ class Ordenador{
            A[ap] = tmp[ap];
 		}
 	}
+    
+    void max_heapify(int *A, int n, int i){
+		int largest = i; 
+    	int left = 2 * i + 1; 
+    	int right = 2 * i + 2; 
+
+		if (left < n && A[left] > A[largest])
+			largest = left;
+
+		if (right < n && A[right] > A[largest])
+			largest = right;
+
+		if (largest != i) {
+			std::swap(A[i], A[largest]); 
+			max_heapify(A, n, largest); 
+		}
+	}
+
+	void buildMaxHeap(int *A, int n) {
+		for (int i = n / 2 - 1; i >= 0; i--) {
+		    max_heapify(A, n, i);
+		}
+  	}
 	
 	public:
 	Ordenador(){};
@@ -92,6 +115,11 @@ class Ordenador{
 	}
 
 	void heapsort(int *A, int n){
+    	buildMaxHeap(A, n);
+    	for (int i = n - 1; i >= 0; i--) {
+        	std::swap(A[0], A[i]);
+        max_heapify(A, i, 0);
+    	}    
 	}
 
 	void quicksort(int *A, int n){
