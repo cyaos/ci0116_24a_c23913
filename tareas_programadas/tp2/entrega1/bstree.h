c942e1c2105425c13e7a61c55cb7ac082d3143a6
Elizabeth Huang <cyhw.eli@Elizabeths-iMac.local>
2024-05-23 20:06:28 -0600

    Documentación de los métodos. También se hicieron los atributos privados.

--- tareas_programadas/tp2/entrega1/bstree.h
+++ tareas_programadas/tp2/entrega1/bstree.h
@@ -11,19 +11,25 @@
 template <typename T>
 class bstnode
 {
-public:
-    // Esta clase es usada por otras clases. 
-    // Modifique los atributos para que sean privados y se accedan solo mediante métodos públicos de la clase.
+private:
+    /// Llave del nodo actual
     T key;
+    /// Punteros al padre, hijo izquierdo e hijo derecho
     bstnode<T> *p, *left, *right;
-    
-    // Constructor por omisión
+
+public:
+    /// @brief Constructor por omisión
     bstnode() {
     };
     
-    // Inicialización de datos miembro
+    /// @brief Inicialización de datos miembro
+    /// @param k llave del nodo actual
+    /// @param w puntero al nodo padre
+    /// @param y puntero al nodo hijo izquierdo
+    /// @param z puntero al nodo hijo derecho
     bstnode(const T& k, bstnode<T> *w = nullptr, bstnode<T> *y = nullptr, bstnode<T> *z = nullptr):key(k), p(w), left(y), right(z) {};
     
+    /// @brief Destructor
     ~bstnode() {
     };
 };
@@ -32,49 +38,74 @@ public:
 template <typename T>
 class bstree
 {
-public:
-    // Esta clase es usada por otras clases. 
-    // Modifique los atributos para que sean privados y se accedan solo mediante métodos públicos de la clase.
-    bstnode<T> *root;    // raíz del árbol
+private:
+    /// Raíz del árbol
+    bstnode<T> *root;
 
+public:
+
+    /// @brief Constructor
+    /// @details Crea un árbol vacío
     bstree() {
-        // Constructor (crea un árbol vacío)
     };
     
+    /// @brief Destructor
+    /// @details Borra el árbol
     ~bstree() {
-        // Destructor (borra el árbol)
     };
     
+    /// @brief Inserta el nodo z en la posición que le corresponde en el árbol.
+    /// @param z nodo a insertar al árbol
     void Insert(bstnode<T>* z) {
-        // Inserta el nodo z en la posición que le corresponde en el árbol.
     };
     
+    /// @brief Recorre en orden el subárbol con raíz x, imprimiendo
+    /// la llave de cada nodo en en una nueva línea de la salida
+    /// estándar después de recorrido el subárbol izquierdo y
+    /// antes de recorrer el subárbol derecho.
+    /// @param x nodo raíz del subárbol
     void InorderWalk(bstnode<T> *x) {
-        // Recorre en orden el subárbol con raíz x, imprimiendo la llave de cada nodo en en una nueva línea de la salida estándar después de recorrido el subárbol izquierdo y antes de recorrer el subárbol derecho.
     };
     
+    /// @brief Busca la llave k recursivamente en el subárbol con raíz x.
+    /// @param x la raíz del subárbol por recorrer
+    /// @param k la llave por buscar
+    /// @return Si la encuentra devuelve un apuntador al nodo que la contiene,
+    /// sino devuelve nullptr.
     bstnode<T>* Search(bstnode<T> *x, const T& k) {
-        // Busca la llave k recursivamente en el subárbol con raíz x. Si la encuentra devuelve un apuntador al nodo que la contiene, sino devuelve nullptr.
     };
     
+    /// @brief Busca la llave k iterativamente en el subarbol con raíz x.
+    /// @param x la raíz del subárbol por recorrer
+    /// @param k la llave por buscar
+    /// @return si la encuentra devuelve un apuntador al nodo que la contiene,
+    /// sino devuelve nullptr.
     bstnode<T>* IterativeSearch(bstnode<T> *x, const T& k) {
-        // Busca la llave k iterativamente en el subarbol con raíz x. Si la encuentra devuelve un apuntador al nodo que la contiene, sino devuelve nullptr.
     };
     
+    /// @brief Busca el nodo que tiene la llave menor
+    /// @param x nodo raíz del subárbol por recorrer
+    /// @return devuelve el nodo con la llave menor
+    /// si el árbol está vacío, devuelve nullptr.
     bstnode<T>* Minimum(bstnode<T> *x) {
-        // Devuelve el nodo que tiene la llave menor. Si el árbol está vacío, devuelve nullptr.
     };
     
+    /// @brief Busca el nodo que tiene la llave mayor
+    /// @param x nodo raíz del subárbol por recorrer
+    /// @return retorna el nodo mayor y si el árbol esta vacío devuelve nullptr.
     bstnode<T>* Maximum(bstnode<T> *x) {
-        // Devuelve el nodo que tiene la llave mayor. Si el árbol esta vacío devuelve nullptr.
     };
     
+    /// @brief Busca el nodo que le sigue al nodo x
+    /// @param x el nodo al cual se le buscara el sucesor 
+    /// @return el nodo cuya llave es la que le sigue a la del nodo x.
+    /// Si no existe el nodo, devuelve nullptr.
     bstnode<T>* Successor(bstnode<T> *x) {
-        // Devuelve el nodo cuya llave es la que le sigue a la del nodo x. Si no existe el nodo, devuelve nullptr.
     };
     
+    /// @brief Saca del árbol la llave contenida en el nodo apuntado por z.
+    /// @param z el nodo por eliminar
     void Delete(bstnode<T>* z) {
-        // Saca del árbol la llave contenida en el nodo apuntado por z.
     };  
 };
 
